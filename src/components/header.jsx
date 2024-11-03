be0f73bb3f9a4548a90167ee398784db7d51aba2
Mahmoud <mahmoud.abdulazizz.1911@gmail.com>
2024-11-03 21:10:57 +0200

    add modal

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signIn, useSession, signOut } from "next-auth/react";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiCamera } from "react-icons/hi";
+import Modal from "react-modal";
 function Header() {
   const { data: session } = useSession();
-  // console.log(session);
+  const filePickerRef = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [caption, setCaption] = useState("");
+  // const [imageFileUploading, setImageFileUploading] = useState(false);
+  // const [postUploading, setPostUploading] = useState(false);
+  const addImageToPost = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (selectedFile) {
+    }
+  }, [selectedFile]);
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <header className="shadow-sm border-b sticky top-0 bg-white z-30 p-3">
       <div className="flex justify-between items-center max-w-6xl mx-auto">
@@ -31,14 +53,18 @@ function Header() {
           className="bg-gray-50 border border-gray-200 rounded text-sm w-full py-2 px-4 max-w-[210px]"
         />
         {session ? (
-          <img
-            className="h-10 w-10 rounded-full cursor-pointer"
-            onClick={signOut}
-            src={session.user.image}
-            // width={40}
-            // height={40}
-            alt={session.user.name}
-          />
+          <div className="flex gap-2 items-center">
+            <IoMdAddCircleOutline
+              className="text-2xl cursor-pointer tranform hover:scale-125 transition duration-300 hover:text-red-600"
+              onClick={() => setIsOpen(true)}
+            />
+            <img
+              src={session.user.image}
+              alt={session.user.name}
+              className="h-10 w-10 rounded-full cursor-pointer"
+              onClick={signOut}
+            />
+          </div>
         ) : (
           <button
             onClick={() => signIn()}
@@ -48,6 +74,63 @@ function Header() {
           </button>
         )}
       </div>
+      {isOpen && (
+        <Modal
+          isOpen={isOpen}
+          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+        >
+          <AiOutlineClose
+            className="cursor-pointer absolute top-2 right-2 hover:text-red-600 transition duration-300"
+            onClick={() => setIsOpen(false)}
+          />
+          <div className="flex flex-col justify-center items-center h-[100%]">
+            {selectedFile ? (
+              <img
+                onClick={() => setSelectedFile(null)}
+                src={imageFileUrl}
+                alt="selected file"
+                className={
+                  `w-full max-h-[250px] object-over cursor-pointer `
+                  // ${imageFileUploading ? "animate-pulse" : ""}
+                }
+              />
+            ) : (
+              <HiCamera
+                onClick={() => filePickerRef.current.click()}
+                className="text-5xl text-gray-400 cursor-pointer"
+              />
+            )}
+            <input
+              hidden
+              ref={filePickerRef}
+              type="file"
+              accept="image/*"
+              onChange={addImageToPost}
+            />
+          </div>
+          <input
+            type="text"
+            maxLength="150"
+            placeholder="Please enter you caption..."
+            className="m-4 border-none text-center w-full focus:ring-0 outline-none"
+            onChange={(e) => setCaption(e.target.value)}
+          />
+          <button
+            // onClick={handleSubmit}
+            disabled={
+              !selectedFile || caption.trim() === ""
+
+              // ||   postUploading ||
+              // imageFileUploading
+            }
+            className="w-full bg-red-600 text-white p-2 shadow-md rounded-lg hover:brightness-105 disabled:bg-gray-200 disabled:cursor-not-allowed disabled:hover:brightness-100"
+          >
+            Upload Post
+          </button>
+        </Modal>
+      )}
     </header>
   );
 }
